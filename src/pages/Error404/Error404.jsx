45e1c6ebf79925da2c7d43e7d5e106ed263672fb
Nathan Degorce <nathandegorce@yahoo.fr>
2026-04-22 06:21:02 +0200

    feat: creation page d'erreur 404

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom"
+import "./Error404.scss"
+
 function Error404() {
-  return <h1>Erreur 404</h1>
+  return (
+    <div className="error-404">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
+    </div>
+  )
 }
 
 export default Error404
